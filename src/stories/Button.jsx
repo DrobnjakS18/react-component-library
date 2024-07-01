4d12d9f9caed588443d892eaf34dba5faf543870
Stefan Drobnjak <drobnjak.stefan18@gmail.com>
2024-07-01 15:51:18 +0200

    init

---
+++ src/stories/Button.jsx
@@ -0,0 +1,70 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './button.css';
+import { Button as MantineButton } from '@mantine/core';
+import '@mantine/core/styles.css';
+import { createTheme, MantineProvider } from '@mantine/core';
+
+const theme = createTheme({
+  /** Put your mantine theme override here */
+});
+
+
+/**
+ * Primary UI component for user interaction
+ */
+export const Button = ({ primary, backgroundColor, size, label, ...props }) => {
+  const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
+  return (
+    // <button
+    //   type="button"
+    //   className={['storybook-button', `storybook-button--${size}`, mode].join(' ')}
+    //   style={backgroundColor && { backgroundColor }}
+    //   {...props}
+    // >
+    //   {label}
+    // </button>
+
+    <MantineProvider theme={theme}>
+      <MantineButton
+        className={['storybook-button', `storybook-button--${size}`, mode].join(' ')}
+        style={backgroundColor && { backgroundColor }}
+        {...props}
+      >
+        {label}
+      </MantineButton>
+    </MantineProvider>
+
+
+  );
+};
+
+Button.propTypes = {
+  /**
+   * Is this the principal call to action on the page?
+   */
+  primary: PropTypes.bool,
+  /**
+   * What background color to use
+   */
+  backgroundColor: PropTypes.string,
+  /**
+   * How large should the button be?
+   */
+  size: PropTypes.oneOf(['small', 'medium', 'large']),
+  /**
+   * Button contents
+   */
+  label: PropTypes.string.isRequired,
+  /**
+   * Optional click handler
+   */
+  onClick: PropTypes.func,
+};
+
+Button.defaultProps = {
+  backgroundColor: null,
+  primary: false,
+  size: 'medium',
+  onClick: undefined,
+};
